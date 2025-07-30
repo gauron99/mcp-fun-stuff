dac3565d56343155afb2a29c66f29fe18e295b01
David Fridrich <fridrich.david19@gmail.com>
2025-07-30 20:23:13 +0200

    rag-tool

---
+++ rag-tool/retrieve.py
@@ -0,0 +1,10 @@
+import requests
+
+
+# Accepts any url link which points to a raw data (*.md/text files etc.)
+# example: https://raw.githubusercontent.com/knative/func/main/docs/function-templates/python.md
+def get_raw_content(url: str) -> str:
+    """ retrieve contents of github raw url as a text """
+    response = requests.get(url)
+    response.raise_for_status() # errors if bad response
+    return response.text
